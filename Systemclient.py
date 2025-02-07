50b018bf34b1ead2d9318e68271a354d097771d6
Sanskar Soni <97246753+Sannutan@users.noreply.github.com>
2025-02-07 23:29:45 +0530

    Update the Code.

--- Systemclient.py
+++ Systemclient.py
@@ -1,16 +1,58 @@
-# Client
 import socket
-s=socket.socket()
-s.bind(('127.0.0.1',3000))
-s.connect(('127.0.0.1',2040))
-while True:
-    # receiving data from server
-    msg=s.recv(1024)
-    print('Server:'+msg.decode('utf-8'))
-    if msg.decode('utf-8')=='bye':
-        break;
-    # Sending data to server
-    data=input('Client:')
-    s.send(bytes(data,'utf-8'))
+import threading
+from datetime import datetime
 
-s.close()
+def receive_messages(sock, username):
+    while True:
+        try:
+            msg = sock.recv(1024).decode('utf-8')
+            if not msg:
+                break
+            timestamp = datetime.now().strftime("%H:%M:%S")
+            print(f'\033[94m[{timestamp}] Server: {msg}\033[0m')
+            if msg.lower() == 'bye':
+                sock.close()
+                break
+        except Exception as e:
+            print(f"\033[91mConnection closed.\033[0m")
+            break
+
+def send_messages(sock, username):
+    while True:
+        try:
+            data = input()
+            timestamp = datetime.now().strftime("%H:%M:%S")
+            sock.send(data.encode('utf-8'))
+            print(f'\033[92m[{timestamp}] You: {data}\033[0m')
+            if data.lower() == 'bye':
+                break
+        except Exception as e:
+            print(f"\033[91mError sending message.\033[0m")
+            break
+
+def main():
+    s = socket.socket()
+    try:
+        s.connect(('127.0.0.1', 2040))
+        # Get username prompt
+        username_prompt = s.recv(1024).decode('utf-8')
+        username = input(username_prompt)
+        s.send(username.encode('utf-8'))
+        
+        # Start threads
+        receive_thread = threading.Thread(target=receive_messages, args=(s, username))
+        send_thread = threading.Thread(target=send_messages, args=(s, username))
+        
+        receive_thread.start()
+        send_thread.start()
+        
+        receive_thread.join()
+        send_thread.join()
+        
+    except Exception as e:
+        print(f"\033[91mError: {e}\033[0m")
+    finally:
+        s.close()
+
+if __name__ == "__main__":
+    main()
--- Systemserver.py
+++ Systemserver.py
@@ -1,23 +1,75 @@
 import socket
-# Create a socket obj
-s = socket.socket()
-#bind the socket with the port number
-s.bind(('127.0.0.1', 2040))
-# listening for a request
-s.listen()
-# accept the connection
-mySocket,clientSocket=s.accept()
-print("Connection established with: "+ str(clientSocket))
+import threading
+from datetime import datetime
 
-while True:
-    # Sending data to the client
-    msg=input('Server:')
-    mySocket.send(bytes(msg, 'utf-8'))
-    if msg=='bye':
-        break;
-    # Receiving data from the client
-    data=mySocket.recv(1024)
-    print('Client:'+ data.decode('utf-8'))
+def handle_client(my_socket, client_address):
+    try:
+        # Request username
+        my_socket.send("Enter your username: ".encode('utf-8'))
+        username = my_socket.recv(1024).decode('utf-8').strip()
+        print(f"\033[92mUser '{username}' connected from {client_address}\033[0m")
+        
+        # Logging setup
+        log_file = open(f"chat_log_{username}.txt", 'a')
+        
+        def log(sender, message):
+            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            log_file.write(f"[{timestamp}] {sender}: {message}\n")
+        
+        def receive():
+            while True:
+                try:
+                    data = my_socket.recv(1024).decode('utf-8')
+                    if not data:
+                        break
+                    timestamp = datetime.now().strftime("%H:%M:%S")
+                    print(f'\033[94m[{timestamp}] {username}: {data}\033[0m')
+                    log(username, data)
+                    if data.lower() == 'bye':
+                        my_socket.close()
+                        break
+                except:
+                    break
 
+        def send():
+            while True:
+                try:
+                    msg = input()
+                    timestamp = datetime.now().strftime("%H:%M:%S")
+                    my_socket.send(msg.encode('utf-8'))
+                    print(f'\033[93m[{timestamp}] You: {msg}\033[0m')
+                    log("Server", msg)
+                    if msg.lower() == 'bye':
+                        my_socket.close()
+                        break
+                except:
+                    break
 
-mySocket.close()
+        receive_thread = threading.Thread(target=receive)
+        send_thread = threading.Thread(target=send)
+        
+        receive_thread.start()
+        send_thread.start()
+        
+        receive_thread.join()
+        send_thread.join()
+        
+    except Exception as e:
+        print(f"\033[91mError: {e}\033[0m")
+    finally:
+        log_file.close()
+        my_socket.close()
+        print(f"\033[91mConnection with {username} closed.\033[0m")
+
+def main():
+    s = socket.socket()
+    s.bind(('127.0.0.1', 2040))
+    s.listen()
+    print("\033[95mServer is listening on port 2040...\033[0m")
+    while True:
+        my_socket, client_address = s.accept()
+        client_thread = threading.Thread(target=handle_client, args=(my_socket, client_address))
+        client_thread.start()
+
+if __name__ == "__main__":
+    main()
